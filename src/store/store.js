b697ff9aead25ac4803360b31883ba82bb620395
Isai Ulate <>
2023-02-16 22:24:43 -0600

    user reducer with Redux Tookkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,36 +1,16 @@
-// import { compose, createStore, applyMiddleware } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-//   Boolean
-// );
-
-// const composeEnhancer =
-//   (process.env.NODE_ENV !== 'production' &&
-//     window &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   blacklist: ['user'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-// Redux Toolkit includes Redux Thunk.
-// If we pass our middleware .. Redux Toolkit will use Redux Thunk by default
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(middleWares),
 });
 
-// export const persistor = persistStore(store);
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,21 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser: (state, action) => {
+      // look like we are mutating the object.. but it is not .. 
+      // the state is a NEW OBJECT .. even though it does not seem like
+      state.currentUser = action.payload;
+    }
   }
-};
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
